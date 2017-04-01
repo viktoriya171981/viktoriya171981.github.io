757e215e4accd6c5009443c70508ca5b23ea1ba3
Viktoriya <viktoriya171981@gmail.com>
2017-04-01 08:40:26 +0300

    site for dad

---
+++ js/building1.js
@@ -0,0 +1,9 @@
+'use strict;'
+function SizeBuilding1Floor1Area () {
+	computeSizeOfMapArea("building1-img", "building1-floor1-svg", "building1-floor1-polygon");
+}
+function SizeBuilding1Floor2Area () {
+	computeSizeOfMapArea("building1-img", "building1-floor2-svg", "building1-floor2-polygon");
+}
+SizeBuilding1Floor1Area();
+SizeBuilding1Floor2Area();
